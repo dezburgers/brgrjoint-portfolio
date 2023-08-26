c6a53c4894942b2854463ee5f5e870b1aea1996e
Dez Burgers <dez@brgrjoint.org>
2023-08-26 16:56:32 -0700

    n

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import linkbanner from "public/link-banner.png"
 
 export default function Document() {
   return (
@@ -10,11 +9,6 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="description" content="Taking Creativity To The Next" />
-        <meta property="og:title" content="Creativity Done Right | BRGR JOINT Productions" />
-        <meta property="og:description" content="Taking Creativity To The Next" />
-        <meta property="og:image" content={linkbanner} />
-        <meta property="og:url" content="www.productions.brgrjoint.org" /> {/* Replace with the actual URL */}
-
       </Head>
       <body className="font-poppins">
         <Main />
